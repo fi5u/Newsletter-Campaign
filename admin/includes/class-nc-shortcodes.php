b49dcdeb2b367fff5f3274cf25fd16e7db0cd90c
Tommy Fisher <tommybfisher@gmail.com>
2014-07-16 14:23:48 +0300

    Size parameters added for feat img

--- admin/includes/class-nc-shortcodes.php
+++ admin/includes/class-nc-shortcodes.php
@@ -60,9 +60,24 @@ class Newsletter_campaign_shortcodes {
     }
 
 
-    public function set_feat_img() {
+    public function set_feat_img($atts) {
         if (has_post_thumbnail($this->post_object->ID)) {
-            return get_the_post_thumbnail($this->post_object->ID, 'thumbnail');
+            // If width or height passed, takes priority over size
+            if (isset($atts['width']) || isset($atts['height'])) {
+                // If only a width or height given then set both to the same
+                $size_arr[] = isset($atts['width']) ? $atts['width'] : $atts['height'];
+                $size_arr[] = isset($atts['height']) ? $atts['height'] : $atts['width'];
+
+                return get_the_post_thumbnail($this->post_object->ID, $size_arr);
+            } else {
+                if (isset($atts['size'])) {
+                    // Use the thumbnail size by name
+                    return get_the_post_thumbnail($this->post_object->ID, $atts['size']);
+                } else {
+                    // Use the thumbnail size by default
+                    return get_the_post_thumbnail($this->post_object->ID, 'thumbnail');
+                }
+            }
         }
     }
 }
