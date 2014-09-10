d7b9d9e414caa33d6c76f471920559947b9dc7a4
Tommy Fisher <tommybfisher@gmail.com>
2014-09-10 16:27:32 +0300

    Shortcode bar accepts args

--- admin/assets/js/template.js
+++ admin/assets/js/template.js
@@ -64,20 +64,38 @@
 
     function addArgsBar(args, shortcode, iteration) {
         var argsBar;
-        argsBar = '<div class"nc-button-bar--args">';
+        argsBar = '<div class"nc-button-bar--args" id="nc-button-bar-args-' + shortcode + '">';
 
         for (var arg = 0; arg < args.length; arg++) {
             argsBar += '<div class="nc-button-bar__arg">';
             argsBar += '<label class="nc-button-bar__arg-label" for="' + args[arg].name + '">' + args[arg].title + '</label>';
-            argsBar += '<input type="text" name="' + args[arg].name + '" id="' + args[arg].name + '" placeholder="Optional">';
+            argsBar += '<input type="text" name="' + args[arg].name + '" id="' + args[arg].name + '" placeholder="Optional" data-arg="' + args[arg].arg + '">';
             argsBar += '</div>';
         };
 
-        argsBar += '<button type="button" class="button" id="nc-shortcode-arg-' + shortcode + '">Insert</button>';
+        argsBar += '<button type="button" class="button" id="nc-shortcode-arg-btn-' + shortcode + '">Insert</button>'; /* TODO: gettext this val */
         argsBar += '</div>';
 
         // Insert into the DOM
         $('.CodeMirror').eq(iteration).prevAll('.nc-button-bar').after(argsBar);
+    }
+
+
+    function populateWithArgs(shortcode) {
+        var args = '',
+            shortcodeComplete;
+        $('#nc-button-bar-args-' + shortcode + '').find('.nc-button-bar__arg').each(function(i) {
+            var argName = $(this).find('input').data('arg'),
+                argVal = $(this).find('input').val();
+
+            if (argVal) {
+                args += ' ' + argName + '="' + argVal + '"';
+            }
+        });
+
+        shortcodeComplete = '[' + shortcode + args + ']';
+
+        console.log(shortcodeComplete);
     }
 
 
@@ -115,6 +133,13 @@
         // Get the instance iteration
         var iteration = $(this).closest('.nc-button-bar').nextAll('.CodeMirror').index('.CodeMirror');
         populateWithShortcode($(this).data('shortcode'), iteration);
+        e.preventDefault();
+    });
+
+    $('body').on('click', '[id^=nc-shortcode-arg-btn-]', function(e) {
+        var idSplit = $(this).attr('id').split('-'),
+            shortcode = idSplit[idSplit.length - 1];
+        populateWithArgs(shortcode);
         e.preventDefault();
     });
 
